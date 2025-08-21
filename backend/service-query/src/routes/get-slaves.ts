b2d04abc3f1f6f263826b6dc4d731118e5d0158f
Juan Rey <sudo.delete.evil@gmail.com>
2025-08-21 13:52:38 -0500

    Update get-slaves.ts

--- backend/service-query/src/routes/get-slaves.ts
+++ backend/service-query/src/routes/get-slaves.ts
@@ -1,53 +1,40 @@
 import { Request, Response, NextFunction } from "express";
 import { formatSlaveData } from "../utils/format";
+import { findAllSlaves } from "../utils/findAllSlaves";
+import database from "../database/client";
 
-export const getSlavesHandler = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+export const getSlavesHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
+    let connection = null
+
     try {
-        const botScheme = {
-            "id": "slave-0",
-            "iteration": 2,
-            "description": "description text",
-            "paused": false,
-            "status": "started",
-            "symbol": "BTCUSDT",
-            "symbol_info": null,
-            "executed": 0,
-            "finished": 0,
-            "leverage": 5,
-            "stop_loss": "0.97",
-            "order_amount": 500,
-            "margin_type": "ISOLATED",
-            "created_at": 1755732627759,
-            "updated_at": 1755733008739,
-            "rule_labels": [
-                "rsi",
-                "squeeze",
-                "adx",
-                "heikin"
-            ],
-            "rule_values": [false, false, false, false]
+        connection = await database.client.getConnection()
+
+        const slaves = await findAllSlaves(connection)
+
+        const response = []
+
+        for (const slave of slaves) {
+            const live = true
+
+            const images = [
+                `https://picsum.photos/id/545/400/600`,
+                `https://picsum.photos/id/343/400/600`,
+                `https://picsum.photos/id/323/400/600`
+            ]
+
+            response.push(formatSlaveData(slave, live, images))
         }
 
-        const live = true
+        res.json({ success: true, message: 'ok', data: response });
 
-        const images = [
-            `https://picsum.photos/id/545/400/600`,
-            `https://picsum.photos/id/343/400/600`,
-            `https://picsum.photos/id/323/400/600`
-        ]
-
-        const scheme = [
-            formatSlaveData(botScheme, live, images)
-        ]
-
-        res.json({ success: true, message: 'ok', data: scheme });
-      
     } catch (error) {
         next(error);
+    } finally {
+        connection?.release?.();
     }
 }
 
