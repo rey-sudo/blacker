a0e40057bd2bdd002d413449ef4197d7de359606
Juan Rey <sudo.delete.evil@gmail.com>
2025-08-24 20:28:33 -0500

    update

--- mobile/lib/controllers/logs_controller.dart
+++ mobile/lib/controllers/logs_controller.dart
@@ -48,6 +48,7 @@ class LogsController extends GetxController {
   void unsubscribeFromSSE() {
     _subscription?.cancel();
     SSEClient.unsubscribeFromSSE();
+    events.clear();
   }
 
   @override
