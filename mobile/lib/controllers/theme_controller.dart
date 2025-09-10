690b6be4323dfff749aac985c3988925c2b17aca
Juan Rey <sudo.delete.evil@gmail.com>
2025-09-10 14:09:24 -0500

    Update theme_controller.dart

--- mobile/lib/controllers/theme_controller.dart
+++ mobile/lib/controllers/theme_controller.dart
@@ -14,9 +14,12 @@ class AppTheme {
   );
 
   static ThemeData darkNight = ThemeData(
+    splashColor: Colors.transparent,
+    highlightColor: Colors.transparent,
+    splashFactory: NoSplash.splashFactory,
     brightness: Brightness.dark,
     fontFamily: 'Inter',
-    scaffoldBackgroundColor:Color(0xFF181A20),
+    scaffoldBackgroundColor: Color(0xFF181A20),
     dividerColor: Color(0xFF848e9c).withAlpha(26),
 
     iconTheme: const IconThemeData(color: Colors.white, size: 24.0),
