8279be56a550750ad8f8fb38286296ec49e473f4
Juan Rey <sudo.delete.evil@gmail.com>
2025-11-14 16:55:27 -0500

    update

---
+++ desktop/app/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+
+<script setup>
+
+const appConfig = useAppConfig()
+
+useHead({
+  title: appConfig.title,
+  meta: [
+    { name: 'description', content: 'red' },
+    { property: 'og:title', content: 'red' }
+  ],
+  link: [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap" rel="stylesheet' }
+  ]
+})
+
+</script>
---
+++ desktop/app/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="layout">
+    <HeaderComp />
+    <main class="content">
+      <NuxtPage :key="$route.fullPath" />
+    </main>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+.layout {
+  background: var(--background-0);
+  width: 100%;
+  height: 100vh;
+  padding: 1%;
+  box-sizing: border-box;
+}
+
+.content {
+  flex: 1;
+}
+</style>
