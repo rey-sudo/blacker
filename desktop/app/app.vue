a0ab79ec363a59572652203e309de52ea5d31bda
Juan Rey <sudo.delete.evil@gmail.com>
2025-11-15 12:14:54 -0500

    update

--- desktop/app/app.vue
+++ desktop/app/app.vue
@@ -18,7 +18,7 @@ useHead({
   link: [
     { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
     { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap" rel="stylesheet' }
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet' }
   ]
 })
 
