198e93d877c3dcd025eaa27808d1b0681cf2388a
Juan Rey <sudo.delete.evil@gmail.com>
2025-11-16 16:46:40 -0500

    Update TabLayout.client.vue

--- desktop/app/components/TabLayout.client.vue
+++ desktop/app/components/TabLayout.client.vue
@@ -8,7 +8,7 @@
 
 <script setup>
 import TabContent from "~/components/TabContent.vue";
-import { ref, onMounted, onBeforeUnmount, markRaw } from "vue";
+import { ref, watch, markRaw } from "vue";
 
 const tabsStore = useTabsStore();
 const tabs = ref([]);
@@ -16,21 +16,22 @@ const tabs = ref([]);
 watch(
   () => tabsStore.tabs,
   (newTabs) => {
-    console.log(newTabs);
+    newTabs.forEach(tab => {
 
-    tabs.value = newTabs.map((tab) => ({
-      id: tab.id,
-      component: markRaw(TabContent),
-    }));
+      if (!tabs.value.find(t => t.id === tab.id)) {
+        tabs.value.push({
+          id: tab.id,
+          component: markRaw(TabContent)
+        });
+      }
+    });
   },
   { deep: true, immediate: true }
 );
 
 function removeTab(id) {
-  tabs.value = tabs.value.filter((t) => t.id !== id);
+  tabs.value = tabs.value.filter(t => t.id !== id);
 }
-
-onMounted(() => {});
 </script>
 
 <style scoped></style>
