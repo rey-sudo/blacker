5c90e8073ae438ef8102ec854784f8ac61690143
Juan Rey <sudo.delete.evil@gmail.com>
2025-11-17 10:54:45 -0500

    Update setup.ts

--- backend/service-market/setup.ts
+++ backend/service-market/setup.ts
@@ -1,7 +1,7 @@
-import { Client } from 'pg';
-import dotenv from 'dotenv';
+import { Client } from "pg";
+import dotenv from "dotenv";
 
-dotenv.config({ path: '.env.development' });
+dotenv.config({ path: ".env.development" });
 
 const {
   DATABASE_HOST,
@@ -16,25 +16,31 @@ const client = new Client({
   port: DATABASE_PORT ? parseInt(DATABASE_PORT) : 5432,
   user: DATABASE_USER,
   password: DATABASE_PASSWORD,
-  database: 'postgres',
+  database: "",
 });
 
 async function createDatabase() {
   try {
     await client.connect();
-    console.log('Connected to PostgreSQL');
+    console.log("Connected to PostgreSQL");
+
+    const dbs = await client.query(`SELECT datname FROM pg_database;`);
+    console.log(
+      "Databases:",
+      dbs.rows.map((r) => r.datname)
+    );
 
     const result = await client.query(`CREATE DATABASE ${DATABASE_NAME}`);
     console.log(`Database "${DATABASE_NAME}" created successfully`);
   } catch (error: any) {
-    if (error.code === '42P04') { 
+    if (error.code === "42P04") {
       console.log(`The database "${DATABASE_NAME}" already exists`);
     } else {
-      console.error('Error creating the database:', error.message);
+      console.error("Error creating the database:", error.message);
     }
   } finally {
     await client.end();
-    console.log('Connection closed');
+    console.log("Connection closed");
   }
 }
 
