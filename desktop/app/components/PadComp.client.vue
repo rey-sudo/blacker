6633bed4ee41179c83eabb640dbcf0e70d79481a
Juan Rey <sudo.delete.evil@gmail.com>
2025-11-18 08:33:29 -0500

    Update PadComp.client.vue

--- desktop/app/components/PadComp.client.vue
+++ desktop/app/components/PadComp.client.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="pad">
-    <Button label="Buy"  />
-    <Button label="Sell" severity="danger" />
+    <div class="price">91,312.00</div>
+
+    <div class="buttons">
+      <Button label="Buy" />
+      <Button label="Sell" severity="danger" />
+    </div>
   </div>
 </template>
 
@@ -9,8 +13,19 @@
 
 <style lang="css" scoped>
 .pad {
-  height: 6rem;
-  padding: 1rem;
+  height: 3rem;
   border: 1px solid var(--border-0);
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+}
+
+.price {
+  font-size: var(--font-size-5);
+  font-weight: 700;
+}
+
+.buttons {
+  margin-left: auto;
 }
 </style>
