0256ad45cc67bcc7eb6a15a2ae252e3bbaeeed11
Juan Rey <sudo.delete.evil@gmail.com>
2025-11-29 13:05:15 -0500

    Update adxRule.ts

--- backend/service-backtest/src/rules/adxRule.ts
+++ backend/service-backtest/src/rules/adxRule.ts
@@ -4,9 +4,8 @@ import { Backtester } from "../index.js";
 export async function adxRule(
   this: Backtester,
   RULE: number,
-  candles: Candle[],
+  candles: Candle[]
 ): Promise<boolean> {
-
   if (!this.state.rule_values[RULE]) {
     const keyLevel = 23;
 
@@ -14,19 +13,21 @@ export async function adxRule(
 
     const lastReversal = reversalPoints.at(-1);
 
-    if (lastReversal) {
-      const range = [
-        candles.at(-1)?.time,
-        candles.at(-2)?.time,
-        candles.at(-3)?.time,
-      ];
-
-      const rule1 = range.includes(lastReversal.time);
-
-      const rule2 = lastReversal.value > keyLevel;
-
-      this.state.rule_values[RULE] = rule1 && rule2;
+    if (!lastReversal) {
+      throw new Error("lastReversal  error");
     }
+
+    const range = [
+      candles.at(-1)?.time,
+      candles.at(-2)?.time,
+      candles.at(-3)?.time,
+    ];
+
+    const rule1 = range.includes(lastReversal.time);
+
+    const rule2 = lastReversal.value > keyLevel;
+
+    this.state.rule_values[RULE] = rule1 && rule2;
   }
 
   return this.state.rule_values[RULE];
