2fd5420a74923b2df25e9ad4f522f94c69c638ea
Juan Rey <sudo.delete.evil@gmail.com>
2025-12-03 19:09:51 -0500

    Update tab.ts

--- desktop/app/stores/tab.ts
+++ desktop/app/stores/tab.ts
@@ -7,7 +7,7 @@ export const createTabStore = (tabId: string) =>
     const interval = ref("h4");
     const window = ref(500);
 
-    const slaveId = ref("slave-8");
+    const slaveId = ref("slave-6");
 
     const candles: any = ref([]);
     const candle = ref(null);
