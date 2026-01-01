5b47a897acd09b4f86cdbd8e4304c4b899e5d64c
Juan Rey <sudo.delete.evil@gmail.com>
2026-01-01 18:55:08 -0500

    Update ChartTimeframeSelector.vue

--- desktop/app/components/ChartTimeframeSelector.vue
+++ desktop/app/components/ChartTimeframeSelector.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="flex justify-center gap-1">
     <UButton color="neutral" size="sm" variant="ghost">15m</UButton>
-    <UButton color="neutral" size="sm" variant="ghost">1h</UButton>
-    <UButton color="neutral" size="sm" variant="ghost">4h</UButton>
-    <UButton color="neutral" size="sm" variant="ghost">1D</UButton>
-    <UButton color="neutral" size="sm" variant="ghost" icon="i-lucide-settings"></UButton>
   </div>
 </template>
 
