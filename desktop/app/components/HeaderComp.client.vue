95a758b2708e35adfd2ff71cc17f613c30d674a4
Juan Rey <sudo.delete.evil@gmail.com>
2026-01-01 20:54:49 -0500

    Update HeaderComp.client.vue

--- desktop/app/components/HeaderComp.client.vue
+++ desktop/app/components/HeaderComp.client.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="header">
-    <img class="logo" src="../assets/icons/logo1.svg" alt="" />
-
     <div class="header-body">
       <Tab :tabId="String(tab.id)" v-for="tab in tabsStore.tabs" :key="tab" />
 
@@ -9,7 +7,7 @@
     </div>
 
     <ColorModeButton />
-    <HeaderSettingsButton style="margin-left: auto;"/>
+    <HeaderSettingsButton style="margin-left: auto" />
   </div>
 </template>
 
@@ -20,11 +18,11 @@ const tabsStore = useTabsStore();
 <style lang="css" scoped>
 .header {
   gap: 1rem;
-  height: var(--header-height);
   display: flex;
   padding: 0 1rem;
   align-items: center;
   box-sizing: border-box;
+  height: var(--header-height);
   background: var(--nav-background);
   border-bottom: 1px solid var(--ui-border);
 }
