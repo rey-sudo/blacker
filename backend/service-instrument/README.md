593b38d744d34c7a15690e2c2fd390bb90312b40
Juan Rey <sudo.delete.evil@gmail.com>
2026-02-05 21:24:16 -0500

    added : service-instrument rust template.

--- backend/service-instrument/README.md
+++ backend/service-instrument/README.md
@@ -1,1 +1,2 @@
-service-instrument
+# rust-template
+ rust-template
---
+++ backend/service-instrument/src/application/api/mod.rs
@@ -0,0 +1,8 @@
+pub mod ohlcv;
+
+use axum::Router;
+use crate::application::state::AppState;
+
+pub fn router() -> Router<AppState> {
+    Router::new().nest("/ohlcv", ohlcv::router())
+}
