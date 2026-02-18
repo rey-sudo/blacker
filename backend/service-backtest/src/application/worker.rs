8ba13faf17f468c0c4e91eaca042ccd02b1e9aa0
Juan Rey <sudo.delete.evil@gmail.com>
2026-02-18 17:10:29 -0500

    update : service-backtest refactorized

--- backend/service-backtest/src/application/worker.rs
+++ backend/service-backtest/src/application/worker.rs
@@ -1,7 +1,7 @@
 use crate::application::event::{ContextId, ControlEvent, InputEvent, OutputEvent};
+use cursor_db::cursor::CursorDB;
 use serde::Deserialize;
-use std::time::Duration;
-use tokio::{sync::mpsc, time};
+use tokio::sync::mpsc;
 
 #[derive(Debug, Deserialize)]
 pub enum Command {
@@ -11,31 +11,24 @@ pub enum Command {
     Delete,
     RunBacktest,
 }
+
+#[derive(Clone)]
+pub struct WorkerState {
+    pub tick_index: usize,
+}
+
 struct Worker {
     context_id: ContextId,
-    state: u64,
+    state: WorkerState,
+    cursor: CursorDB
 }
 
 impl Worker {
-    fn new(context_id: ContextId) -> Self {
+    fn new(context_id: ContextId, state: WorkerState, cursor: CursorDB) -> Self {
         Self {
             context_id,
-            state: 0,
-        }
-    }
-
-    fn apply(&mut self, payload: Vec<u8>) {
-        self.state += payload.len() as u64;
-    }
-
-    fn maybe_emit(&mut self) -> Option<OutputEvent> {
-        if self.state > 0 && self.state % 10 == 0 {
-            Some(OutputEvent {
-                context_id: self.context_id.clone(),
-                payload: format!("signal-{}", self.state).into_bytes(),
-            })
-        } else {
-            None
+            state,
+            cursor,
         }
     }
 }
@@ -46,55 +39,58 @@ pub async fn worker_loop(
     mut rx: mpsc::Receiver<InputEvent>,
     tx_output: mpsc::Sender<OutputEvent>,
 ) {
-    let mut worker = Worker::new(context_id.clone());
-    let mut interval = time::interval(Duration::from_millis(200));
+    let data_path: &str = "../data/data.cdb";
+    let index_path: &str = "../data/index.cdbi";
+
+    let state: WorkerState = WorkerState { tick_index: 0 };
+
+    let mut cursor: CursorDB = match CursorDB::open_or_create(data_path, index_path) {
+        Ok(cursor_db) => {
+            println!("CursorDB abierto o creado correctamente");
+            cursor_db
+        }
+        Err(e) => {
+            eprintln!("Error al abrir o crear CursorDB: {:?}", e);
+            return; 
+        }
+    };
+
+    let mut worker: Worker = Worker::new(context_id.clone(), state, cursor);
 
     loop {
         tokio::select! {
-        
-        Some(event) = rx.recv() => {
-        
-            worker.apply(event.params.into());
 
-            match event.command {
-                Command::Setup => {
-                    println!("Handling Setup command");
-                    // lógica para inicialización
-                }
+            Some(event) = rx.recv() => {
 
-                Command::Start => {
-                    println!("Handling Start command");
-                    // lógica para iniciar proceso
-                }
+                match event.command {
+                    Command::Setup => {
+                        println!("Handling Setup command");
+                        println!("Params: {}", event.params);
+                    }
 
-                Command::Stop => {
-                    println!("Handling Stop command");
-                    // lógica para detener proceso
-                }
+                    Command::Start => {
+                        println!("Handling Start command");
+                        // lógica para iniciar proceso
+                    }
 
-                Command::Delete => {
-                    println!("Handling Delete command");
-                    // lógica para eliminar recursos
-                }
+                    Command::Stop => {
+                        println!("Handling Stop command");
+                        // lógica para detener proceso
+                    }
 
-                Command::RunBacktest => {
-                    println!("Handling RunBacktest command");
-                    // lógica para ejecutar backtest
-                }
-            }
-        
-        }
+                    Command::Delete => {
+                        println!("Handling Delete command");
+                        // lógica para eliminar recursos
+                    }
 
-        _ = interval.tick() => {
-                if let Some(out) = worker.maybe_emit() {
-                    if tx_output.send(out).await.is_err() {
-                                    break;
+                    Command::RunBacktest => {
+                        println!("Handling RunBacktest command");
+                        // lógica para ejecutar backtest
                     }
                 }
+
             }
-            
+
         }
     }
-
-    println!("Worker {} terminated", context_id);
 }
