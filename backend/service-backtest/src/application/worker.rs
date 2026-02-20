473aa6e27305a6d9b9e821be1023267193ecf5ed
Juan Rey <sudo.delete.evil@gmail.com>
2026-02-19 21:28:14 -0500

    update : service-backtest worker setup()

--- backend/service-backtest/src/application/worker.rs
+++ backend/service-backtest/src/application/worker.rs
@@ -1,7 +1,61 @@
 use crate::application::event::{ContextId, ControlEvent, InputEvent, OutputEvent};
-use cursor_db::cursor::CursorDB;
+use cursor_db::{cursor::CursorDB, record::Record};
 use serde::Deserialize;
+use serde_json;
 use tokio::sync::mpsc;
+use tracing::{debug, error, info};
+
+#[derive(Debug)]
+pub enum WorkerError {
+    AlreadyInitialized,
+    InvalidParams,
+    CursorInitFailed,
+    EmptyDataset
+}
+
+#[derive(Deserialize)]
+struct SetupParams {
+    symbol: String
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Symbol(String);
+
+impl Symbol {
+    pub fn new(value: impl Into<String>) -> Self {
+        Self(value.into())
+    }
+
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(u8)]
+pub enum Timeframe {
+    M1 = 1,
+    M5 = 2,
+    M15 = 3,
+    M30 = 4,
+    H1 = 5,
+    H4 = 6,
+    D1 = 7,
+}
+
+impl Timeframe {
+    pub const fn as_seconds(self) -> u64 {
+        match self {
+            Timeframe::M1 => 60,
+            Timeframe::M5 => 300,
+            Timeframe::M15 => 900,
+            Timeframe::M30 => 1800,
+            Timeframe::H1 => 3600,
+            Timeframe::H4 => 14_400,
+            Timeframe::D1 => 86_400,
+        }
+    }
+}
 
 #[derive(Debug, Deserialize)]
 pub enum Command {
@@ -14,22 +68,61 @@ pub enum Command {
 
 #[derive(Clone)]
 pub struct WorkerState {
-    pub tick_index: usize,
+    pub current_index: Record,
 }
 
 struct Worker {
     context_id: ContextId,
-    state: WorkerState,
-    cursor: CursorDB
+    symbol: Option<Symbol>,
+    state: Option<WorkerState>,
+    cursor: Option<CursorDB>,
 }
 
 impl Worker {
-    fn new(context_id: ContextId, state: WorkerState, cursor: CursorDB) -> Self {
+    fn new(context_id: ContextId) -> Self {
         Self {
             context_id,
-            state,
-            cursor,
+            symbol: None,
+            state: None,
+            cursor: None,
         }
+    }
+}
+
+impl Worker {
+    pub fn setup(&mut self, event: InputEvent) -> Result<(), WorkerError> {
+
+        if self.state.is_some() {
+            return Err(WorkerError::AlreadyInitialized);
+        }
+
+        info!("{:?}", event.params);
+
+        let params: SetupParams =
+            serde_json::from_str(&event.params).map_err(|_| WorkerError::InvalidParams)?;
+
+        let symbol: Symbol = Symbol::new(params.symbol);
+
+        let data_path: &str = "data/data.cdb";      //symbol
+        let index_path: &str = "data/index.cdbi";
+
+        let mut cursor: CursorDB =
+            CursorDB::new(data_path, index_path).map_err(|_| WorkerError::CursorInitFailed)?;
+
+        let current_index = cursor
+            .move_to_first()
+            .map_err(|_| WorkerError::CursorInitFailed)?
+            .ok_or(WorkerError::EmptyDataset)?;
+
+        info!("{:?}", current_index);
+
+        let state: WorkerState = WorkerState { current_index };
+
+        self.symbol = Some(symbol);
+        self.cursor = Some(cursor);
+        self.state = Some(state);
+
+        Ok(())
     }
 }
 
@@ -39,23 +132,7 @@ pub async fn worker_loop(
     mut rx: mpsc::Receiver<InputEvent>,
     tx_output: mpsc::Sender<OutputEvent>,
 ) {
-    let data_path: &str = "../data/data.cdb";
-    let index_path: &str = "../data/index.cdbi";
-
-    let state: WorkerState = WorkerState { tick_index: 0 };
-
-    let mut cursor: CursorDB = match CursorDB::open_or_create(data_path, index_path) {
-        Ok(cursor_db) => {
-            println!("CursorDB abierto o creado correctamente");
-            cursor_db
-        }
-        Err(e) => {
-            eprintln!("Error al abrir o crear CursorDB: {:?}", e);
-            return; 
-        }
-    };
-
-    let mut worker: Worker = Worker::new(context_id.clone(), state, cursor);
+    let mut worker: Worker = Worker::new(context_id.clone());
 
     loop {
         tokio::select! {
@@ -64,8 +141,14 @@ pub async fn worker_loop(
 
                 match event.command {
                     Command::Setup => {
-                        println!("Handling Setup command");
-                        println!("Params: {}", event.params);
+                        match worker.setup(event) {
+                            Ok(_) => {
+                                info!("Worker setup complete");
+                            }
+                            Err(e) => {
+                                error!("Setup failed: {:?}", e);
+                            }
+                        }
                     }
 
                     Command::Start => {
@@ -94,3 +177,41 @@ pub async fn worker_loop(
         }
     }
 }
+
+/*
+
+    let data_path: &str = "../data/data.cdb";
+    let index_path: &str = "../data/index.cdbi";
+
+    let mut cursor: CursorDB = match CursorDB::new(data_path, index_path) {
+       Ok(c) => {
+           println!("CursorDB abierto o creado correctamente");
+           c
+       }
+       Err(e) => {
+           eprintln!("Error al abrir o crear CursorDB: {:?}", e);
+           return;
+       }
+   };
+
+   match cursor.move_to_first() {
+       Ok(_) => {
+           println!("Cursor posicionado en el inicio con éxito.");
+           // Aquí podrías llamar a db.next() para leer el primer registro
+       }
+       Err(e) => {
+           eprintln!("Error al mover el cursor: {}", e);
+           // Aquí decides si quieres hacer un 'return' o intentar otra cosa
+       }
+   }
+
+   let current_index = cursor.current()?; // Si hay error, la función termina aquí
+
+   match current_index {
+       Some(record) => println!("Datos: {:?}", record.payload),
+       None => println!("Cursor en posición nula."),
+   }
+
+
+
+*/
