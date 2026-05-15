5f0a8b6067442b8f59ab24b1b60bac277d4dbaa1
Juan Rey <sudo.delete.evil@gmail.com>
2026-05-14 23:03:55 -0500

    update

--- backend/services/backtest/src/main.rs
+++ backend/services/backtest/src/main.rs
@@ -1,99 +1,46 @@
-use csv::{ByteRecord, ReaderBuilder};
-use cursor_db::cursor::CursorDB;
-use rkyv::{Archive, Serialize};
-use tracing::info;
-use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::{fs, path::Path};
+use backtest::{app::model::Trade, utils::csv_to_cursor};
+use cursor_db::{cursor::CursorDB, record::Record};
+use std::{thread, time::Duration};
 
-#[derive(Archive, Serialize)]
-pub struct Trade {
-    id: u64,
-    price: f64,
-    qty: f64,
-    quote_qty: f64,
-    time: i64,
-    is_buyer_maker: bool,
-    //is_best_match: bool,
-}
-
-fn iterate_csv_file(mut db: CursorDB) -> Result<(), Box<dyn Error>> {
-    let file: File = File::open("./input/BTCUSDT-trades-2026-05-06_grouped.csv")?;
-
-    let reader: BufReader<File> = BufReader::with_capacity(1024 * 1024 * 16, file);
-
-    let mut csv: csv::Reader<BufReader<File>> =
-        ReaderBuilder::new().has_headers(false).from_reader(reader);
-
-    let mut row: ByteRecord = ByteRecord::new();
-    
-    println!("Running csv iteration");
-
-    while csv.read_byte_record(&mut row)? {
-        let trade: Trade = Trade {
-            id: std::str::from_utf8(&row[0])?.parse()?,
-            price: std::str::from_utf8(&row[1])?.parse()?,
-            qty: std::str::from_utf8(&row[2])?.parse()?,
-            quote_qty: std::str::from_utf8(&row[3])?.parse()?,
-            time: std::str::from_utf8(&row[4])?.parse()?,
-            is_buyer_maker: row[5] == b"True"[..],
-            //is_best_match: row[6] == b"True"[..],
-        };
-
-        let payload: rkyv::util::AlignedVec = rkyv::to_bytes::<rkyv::rancor::Error>(&trade)?;
-
-        match db.insert(trade.time, &payload) {
-            Ok(_) => {
-                //println!("✔ Record Saved: TS {}", trade.time);
+fn iterate_cursor(mut db: CursorDB) {
+    loop {
+        match db.next() {
+            Ok(Some(Record { timestamp, payload })) => {
+                
+                match bincode::deserialize::<Trade>(&payload) {
+                    Ok(trade) => {
+                        println!("Trade: {:?}", timestamp);
+                    }
+                    Err(e) => {
+                        eprintln!("Deserialize error: {}", e);
+                    }
+                }
+            }
+            Ok(None) => {
+                println!("No more records following");
+                break;
             }
             Err(e) => {
-                eprintln!("🗙 Insert Error: {}", e);
+                eprintln!("Moved to next error: {}", e);
+                break;
             }
         }
+
+        thread::sleep(Duration::from_secs(1));
     }
-
-    match db.move_to_first() {
-        Ok(Some(record)) => {
-            println!("Moved to first: {}", record.timestamp);
-        }
-
-        Ok(None) => {
-            println!("There is no first position");
-        }
-
-        Err(e) => {
-            eprintln!("Moved to first error: {}", e);
-        }
-    }
-
-    Ok(())
-}
-
-fn delete_input_content(
-    data_path: &str,
-    index_path: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    for path in &[data_path, index_path] {
-        if Path::new(path).exists() {
-            fs::remove_file(path)?;
-            println!("Deleted previous file: {}", path);
-        }
-    }
-
-    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Define file paths
+    let csv_path: &str = "./input/BTCUSDT-trades-2026-05-06_grouped.csv";
+
     let data_path: &str = "./data/data.cdb";
     let index_path: &str = "./data/index.cdbi";
 
-    delete_input_content(data_path, index_path)?;
-
     let mut db: CursorDB = CursorDB::new(data_path, index_path)?;
 
-    iterate_csv_file(db)?;
+    //csv_to_cursor(db, csv_path)?;
+
+    iterate_cursor(db);
 
     Ok(())
 }
---
+++ backend/services/backtest/src/utils/mod.rs
@@ -0,0 +1,2 @@
+pub mod csv_to_cursor;
+pub use csv_to_cursor::csv_to_cursor;
