68cfb19d7af6efd25d740fb9e200f399a0055546
Juan Rey <juanjo9841@gmail.com>
2026-07-07 16:48:44 -0500

    update

--- backend/services/backtest/test/master/src/main.rs
+++ backend/services/backtest/test/master/src/main.rs
@@ -1,22 +1,19 @@
 use master::server::start_http_server;
 use master::state::AppState;
 use master::tasks::{start_master_monitor, start_slave_monitor};
+use std::sync::Arc;
 use tickdb::binary::BinaryFile;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let db: BinaryFile = BinaryFile::open("./input/ticks.bin")?;
+    let tick_data: Arc<BinaryFile> = Arc::new(BinaryFile::open("./input/ticks.bin")?);
 
-    let mut cursor = db.cursor();
-
-    println!("{:?}", cursor.current());
-
-    let state: AppState = AppState::new();
+    let state: AppState = AppState::new(tick_data);
 
     start_master_monitor(state.clone());
-    start_slave_monitor(state.clone());
+    //start_slave_monitor(state.clone());
     start_http_server(state).await;
 
     Ok(())
--- backend/services/backtest/test/master/src/state.rs
+++ backend/services/backtest/test/master/src/state.rs
@@ -4,13 +4,34 @@ use crate::{
 };
 use serde::Serialize;
 use std::{collections::HashMap, sync::Arc};
+use tickdb::{binary::BinaryFile, trade::Trade};
 use tokio::sync::RwLock;
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Clone, Serialize)]
 pub struct MasterState {
     pub status: MasterStatus,
     pub slaves: HashMap<SlaveId, ConnectedSlaveState>,
     pub version: u64,
+    #[serde(skip)]
+    pub tick_data: Arc<BinaryFile>,
+    pub tick_index: usize,
+}
+
+impl MasterState {
+    #[inline]
+    pub fn trade(&self, index: usize) -> Option<&Trade> {
+        self.tick_data.trade(index)
+    }
+
+    #[inline]
+    pub fn current_tick(&self) -> Option<&Trade> {
+        self.tick_data.trade(self.tick_index)
+    }
+
+    #[inline]
+    pub fn has_next_tick(&self) -> bool {
+        self.tick_index + 1 < self.tick_data.len()
+    }
 }
 
 #[derive(Clone)]
@@ -19,12 +40,14 @@ pub struct AppState {
 }
 
 impl AppState {
-    pub fn new() -> Self {
+    pub fn new(tick_data: Arc<BinaryFile>) -> Self {
         Self {
             master: Arc::new(RwLock::new(MasterState {
                 status: MasterStatus::Pending,
                 slaves: HashMap::new(),
-                version: 10
+                version: 10,
+                tick_data,
+                tick_index: 0,
             })),
         }
     }
