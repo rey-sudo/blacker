3439a017e545ac6f60cd78860aaca4b604f83c28
Juan Rey <juanjo9841@gmail.com>
2026-07-07 15:52:49 -0500

    update

--- backend/services/backtest/test/master/src/main.rs
+++ backend/services/backtest/test/master/src/main.rs
@@ -1,14 +1,24 @@
 use master::server::start_http_server;
 use master::state::AppState;
 use master::tasks::{start_master_monitor, start_slave_monitor};
+use tickdb::binary::BinaryFile;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let state: master::state::AppState = AppState::new();
+    let tickdb: BinaryFile = BinaryFile::open("./input/ticks.bin")?;
+
+    tracing::info!(
+        trades = tickdb.len(),
+        "Tick database loaded"
+    );
+
+    let state: AppState = AppState::new();
 
     start_master_monitor(state.clone());
     start_slave_monitor(state.clone());
     start_http_server(state).await;
+
+    Ok(())
 }
