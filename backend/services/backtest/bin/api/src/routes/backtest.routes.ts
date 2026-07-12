ad74b6006d9d1dd1a7547a47904eb8749c8d7a4f
Juan Rey <juanjo9841@gmail.com>
2026-07-12 14:42:47 -0500

    update

--- backend/services/backtest/bin/api/src/routes/backtest.routes.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { backtestHandler } from "../handlers/backtest.handler.js";
-
-export async function backtestRoutes(app: FastifyInstance) {
-  app.get("/ws", { websocket: true }, backtestHandler);
-}
