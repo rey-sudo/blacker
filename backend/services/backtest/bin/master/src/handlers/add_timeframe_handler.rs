a66d87f61d4b491a5d18116c13c2df6d81660cba
Juan Rey <juanjo9841@gmail.com>
2026-08-14 23:29:24 -0500

    Update add_timeframe_handler.rs

--- backend/services/backtest/bin/master/src/handlers/add_timeframe_handler.rs
+++ backend/services/backtest/bin/master/src/handlers/add_timeframe_handler.rs
@@ -1,4 +1,22 @@
-use crate::{master::state::{AppState, MasterState}, slaves::{engine::{Timeframe}}};
+// BLACKER
+// Copyright (C) 2026 Juan José Caballero Rey
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::{
+    master::state::{AppState, MasterState},
+    slaves::engine::Timeframe,
+};
 use axum::{Json, extract::State, http::StatusCode};
 use serde::{Deserialize, Serialize};
 use tokio::sync::RwLockWriteGuard;
@@ -11,27 +29,33 @@ pub struct Request {
 
 #[derive(Serialize)]
 pub struct Response {
-    pub ok: bool
+    pub ok: bool,
 }
 
 pub async fn add_timeframe_handler(
     State(state): State<AppState>,
     Json(req): Json<Request>,
 ) -> (StatusCode, Json<Response>) {
-    let response: Response = {
-        let mut master: RwLockWriteGuard<'_, MasterState> = state.master.write().await;
+    let mut master: RwLockWriteGuard<'_, MasterState> = state.master.write().await;
 
-        master.engine_state.timeframes.insert(req.timeframe.id.clone(), req.timeframe);
-        master.config_hash = Uuid::now_v7().to_string();
-        
-        drop(master);
-        
-        let _ = state.publish_master_state().await;
+    if master
+        .engine_state
+        .timeframes
+        .contains_key(&req.timeframe.id)
+    {
+        return (StatusCode::CONFLICT, Json(Response { ok: false }));
+    }
 
-        Response {
-            ok: true
-        }
-    };
+    master
+        .engine_state
+        .timeframes
+        .insert(req.timeframe.id.clone(), req.timeframe);
 
-    (StatusCode::OK, Json(response))
+    master.config_hash = Uuid::now_v7().to_string();
+
+    drop(master);
+
+    let _ = state.publish_master_state().await;
+
+    (StatusCode::CREATED, Json(Response { ok: true }))
 }
