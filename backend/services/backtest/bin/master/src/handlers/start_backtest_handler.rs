72f17a7a456f46deb312bed07cf3a3c44404a275
Juan Rey <juanjo9841@gmail.com>
2026-08-21 16:54:31 -0500

    Update start_backtest_handler.rs

--- backend/services/backtest/bin/master/src/handlers/start_backtest_handler.rs
+++ backend/services/backtest/bin/master/src/handlers/start_backtest_handler.rs
@@ -1,19 +1,70 @@
+// BLACKER
+// Copyright (C) 2026 Juan José Caballero Rey
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use crate::master::state::{AppState, MasterState, MasterStatus, ReplayStatus};
-use axum::{extract::State, http::StatusCode};
+use axum::{Json, extract::State, http::StatusCode};
+use serde::{Deserialize, Serialize};
 use tokio::sync::RwLockWriteGuard;
 use tracing::info;
 
+/// Body of the request.
+#[derive(Debug, Deserialize)]
+pub struct Request {}
+
+/// Response of the request.
+#[derive(Serialize)]
+pub struct Response {
+    pub success: bool,
+    pub message: String,
+}
+
+/// Start the backtest handler.
 pub async fn start_backtest_handler(
     State(state): State<AppState>,
-) -> Result<StatusCode, StatusCode> {
+    Json(req): Json<Request>,
+) -> (StatusCode, Json<Response>) {
     let mut master: RwLockWriteGuard<'_, MasterState> = state.master.write().await;
 
     if master.status != MasterStatus::Ready {
-        return Err(StatusCode::PRECONDITION_FAILED);
+        return (
+            StatusCode::PRECONDITION_FAILED,
+            Json(Response {
+                success: false,
+                message: "Master is not ready.".to_string(),
+            }),
+        );
     }
 
     if master.replay_status != ReplayStatus::Stopped {
-        return Err(StatusCode::CONFLICT);
+        return (
+            StatusCode::CONFLICT,
+            Json(Response {
+                success: false,
+                message: "Replay is not stopped.".to_string(),
+            }),
+        );
+    }
+
+    if master.engine_state.timeframes.is_empty() {
+        return (
+            StatusCode::CONFLICT,
+            Json(Response {
+                success: false,
+                message: "At least one timeframe is required.".to_string(),
+            }),
+        );
     }
 
     master.replay_status = ReplayStatus::Running;
@@ -22,7 +73,13 @@ pub async fn start_backtest_handler(
 
     let _ = state.publish_master_state().await;
 
-    info!("Backtest running.");
+    info!("Replay is running.");
 
-    Ok(StatusCode::OK)
+    (
+        StatusCode::OK,
+        Json(Response {
+            success: true,
+            message: "The backtest has started.".to_string(),
+        }),
+    )
 }
